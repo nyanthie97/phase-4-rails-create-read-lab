83542a838369e75c0ea4b760f80b09bb70e86772
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-10-05 01:53:50 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :plants
+  resources :plants, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'plants', to: 'plants#index'
+  get '/plants/:id', to: 'plants#show'
 end
